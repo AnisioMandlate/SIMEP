63308b5539e43a2f028e52ca21f91e68ad045619
Anisio Mandlate <anisiomandlate@gmail.com>
2020-06-30 12:26:20 +0200

    feat: Add the notifications page

--- src/components/Notifications/Notification.js
+++ src/components/Notifications/Notification.js
@@ -1,20 +1,20 @@
-// import React from "react";
-// import styles from "./Notification.module.css";
-// const Notification = () => {
-//   return (
-//     <div className={styles.notification}>
-//       <div className={styles["notification-header"]}>
-//         <div>
-//           <p>05 Notificações</p>
-//         </div>
-//         <div className={styles.options}>
-//           <p>Selecionar</p>
-//           <p>Filtrar</p>
-//         </div>
-//       </div>
-//       <hr />
-//     </div>
-//   );
-// };
+import React from "react";
+import styles from "./Notification.module.css";
+const Notification = () => {
+  return (
+    <div className={styles.notification}>
+      <div className={styles["notification-header"]}>
+        <div>
+          <p>05 Notificações</p>
+        </div>
+        <div className={styles.options}>
+          <p>Selecionar</p>
+          <p>Filtrar</p>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+};
 
-// export default Notification;
+export default Notification;
