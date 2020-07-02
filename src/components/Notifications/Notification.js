85a42d003afadee78c09a7989c92561c2a8626e4
Anisio Mandlate <anisiomandlate@gmail.com>
2020-07-02 15:45:51 +0200

    feat: Change the layout of the notifications body to table instead of div's

--- src/components/Notifications/Notification.js
+++ src/components/Notifications/Notification.js
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./Notification.module.css";
 import deleteIcon from "../../assets/delete.svg";
 import mail from "../../assets/mail.svg";
+import readMail from "../../assets/read-mail.svg";
 
 const Notification = () => {
   return (
     <div className={styles.notification}>
       <div className={styles["notification-header"]}>
         <div>
-          <p>05 Notificações</p>
+          <p className={styles["notification-para"]}>05 Notificações</p>
         </div>
         <div className={styles.options}>
           <p>Selecionar</p>
@@ -17,21 +18,115 @@ const Notification = () => {
       </div>
       <hr />
       <div className={styles["notification-body"]}>
-        <div className={styles.message}>
-          <div>
-            <img src={mail} alt="Message" />
-          </div>
-          <div>
-            <h4>Novo Registo de Edificio</h4>
-            <p>
-              Predio das Torres Vervelhas, Bairro Maputo Museu, distrito de
-              Kamavoto
-            </p>
-          </div>
-          <p>20 de Junho de 2020</p>
-          <p>13:00:56</p>
-          <img src={deleteIcon} alt="Deletar a mensagem" />
-        </div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <img src={mail} alt="Message" className={styles.icons} />
+              </td>
+              <td>
+                <h4>Novo Registo de Edifício</h4>
+                <p>
+                  Predio das Torres Vervelhas, Bairro Maputo Museu, distrito de
+                  Kamavoto
+                </p>
+              </td>
+              <td>20 de Junho de 2020</td>
+              <td>13:00:56</td>
+              <td>
+                <img
+                  src={deleteIcon}
+                  alt="Deletar a mensagem"
+                  className={styles.icons}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={mail} alt="Message" className={styles.icons} />
+              </td>
+              <td>
+                <h4>Novo Registo de Edifício</h4>
+                <p>
+                  Predio das Torres Vervelhas, Bairro Maputo Museu, distrito de
+                  Kamavoto
+                </p>
+              </td>
+              <td>20 de Junho de 2020</td>
+              <td>13:00:56</td>
+              <td>
+                <img
+                  src={deleteIcon}
+                  alt="Deletar a mensagem"
+                  className={styles.icons}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={readMail} alt="Message" className={styles.icons} />
+              </td>
+              <td>
+                <h4>Novo Registo de Edifício</h4>
+                <p>
+                  Predio das Torres Vervelhas, Bairro Maputo Museu, distrito de
+                  Kamavoto
+                </p>
+              </td>
+              <td>20 de Junho de 2020</td>
+              <td>13:00:56</td>
+              <td>
+                <img
+                  src={deleteIcon}
+                  alt="Deletar a mensagem"
+                  className={styles.icons}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={mail} alt="Message" className={styles.icons} />
+              </td>
+              <td>
+                <h4>Alteração de dados de registo de usuario</h4>
+                <p>
+                  Marcos Antônio, Departamento de Operações, Técnico de Registo
+                  e acompanhamento do SIMEP
+                </p>
+              </td>
+              <td>31 de Marco de 2020</td>
+              <td>12:00:00</td>
+              <td>
+                <img
+                  src={deleteIcon}
+                  alt="Deletar a mensagem"
+                  className={styles.icons}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={readMail} alt="Message" className={styles.icons} />
+              </td>
+              <td>
+                <h4>Alteração de dados de registo de usuario</h4>
+                <p>
+                  Claudio Mendes, Departamento de Operações, Técnico de Registo
+                  e acompanhamento do SIMEP
+                </p>
+              </td>
+              <td>29 de Marco de 2020</td>
+              <td>10:00:20</td>
+              <td>
+                <img
+                  src={deleteIcon}
+                  alt="Deletar a mensagem"
+                  className={styles.icons}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
