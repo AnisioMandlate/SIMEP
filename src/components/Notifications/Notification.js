270ea47814d2e35044e5ee123494e209e22d1380
Anisio Mandlate <anisiomandlate@gmail.com>
2020-07-02 10:24:06 +0200

    feat: Add the message content on the notifications

--- src/components/Notifications/Notification.js
+++ src/components/Notifications/Notification.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./Notification.module.css";
+import deleteIcon from "../../assets/delete.svg";
+import mail from "../../assets/mail.svg";
+
 const Notification = () => {
   return (
     <div className={styles.notification}>
@@ -13,6 +16,23 @@ const Notification = () => {
         </div>
       </div>
       <hr />
+      <div className={styles["notification-body"]}>
+        <div className={styles.message}>
+          <div>
+            <img src={mail} alt="Message" />
+          </div>
+          <div>
+            <h4>Novo Registo de Edificio</h4>
+            <p>
+              Predio das Torres Vervelhas, Bairro Maputo Museu, distrito de
+              Kamavoto
+            </p>
+          </div>
+          <p>20 de Junho de 2020</p>
+          <p>13:00:56</p>
+          <img src={deleteIcon} alt="Deletar a mensagem" />
+        </div>
+      </div>
     </div>
   );
 };
