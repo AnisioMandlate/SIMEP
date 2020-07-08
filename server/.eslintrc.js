590a9c37d564e91384848b9442aae83030fc31de
Luis Saiete <luistsaiete@gmail.com>
2020-07-08 22:43:15 +0200

    configurando eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		es2020: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-base',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 11,
+		sourceType: 'module',
+	},
+	plugins: [
+		'@typescript-eslint',
+	],
+	rules: {
+		"indent": [2, "tab"],
+		"no-tabs": 0
+	},
+};
